35878a0bec8d73c659e4e61f505c913e5873eed4
Zoran Matovic <matovic.zoran73@gmail.com>
2023-10-15 21:59:30 +0200

    update

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@
         static void Main(string[] args)
         {
 
-            Circle c1 = new(5);       //instantiate objects 
-            Circle c2 = new(6);
-            Triangle t = new(5, 6);
+            Circle c1 = new Circle(5);       //instantiate objects 
+            Circle c2 = new Circle(6);
+            Triangle t1 = new Triangle(5, 6);
 
 
             // Call our methods
             Console.WriteLine($"Radius of circle1 is: {c1.Radius}cm");
             Console.WriteLine($"Radius of circle2 is: {c2.Radius}cm");
-            Console.WriteLine($"Triangle has a base of {t.Base}cm and a height of {t.Height}cm");
+            Console.WriteLine($"Triangle has a base of {t1.Base}cm and a height of {t1.Height}cm");
             Console.WriteLine("-----------------------------------------------");
             Console.WriteLine($"Area of circle1 is: {c1.GetArea(c1.Radius)}cm²");
             Console.WriteLine($"Area of circle2 is: {c2.GetArea(c2.Radius)}cm²");
@@ -25,8 +25,8 @@
             Console.WriteLine($"Surface area of a sphere with radius 6cm is: {c2.GetSphereArea(c2.Radius)}cm²");
             Console.WriteLine($"Volume of a sphere with radius 6cm is: {c2.GetSphereVolume(c2.Radius)}cm³");
             Console.WriteLine("------------------------------------------------");
-            Console.WriteLine($"Area of triangle is: {t.GetArea(t.Height, t.Base)}cm²");
-            Console.WriteLine($"Circumference of a right triangle is: {t.GetCircumference(t.Height, t.Base)}cm");
+            Console.WriteLine($"Area of triangle is: {t1.GetArea(t1.Height, t1.Base)}cm²");
+            Console.WriteLine($"Circumference of a right triangle is: {t1.GetCircumference(t1.Height, t1.Base)}cm");
 
 
 
